a558d472b2e546f5cf58fcb29f4de4d8030a4c5e
FWC <felix@wortmanncallejon.de>
2024-12-04 21:52:29 +0100

    Day template

---
+++ dayx.R
@@ -0,0 +1,24 @@
+pacman::p_load(dplyr, readr, purrr, adventr)
+
+input <- read_advent(day = 2, year = 2024)
+
+
+problem_one <- function(x) {
+
+  return()
+}
+
+
+problem_two <- function(x) {
+
+  return()
+}
+
+
+problem_one(input) |>
+    submit_advent(level = 1, day = 2, year = 2024)
+
+problem_two(input) |>
+    submit_advent(level = 2, day = 2, year = 2024)
+
+
